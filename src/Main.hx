275167e80fe714cff83ddf3ffa2dabeb7552c3fe
unknown <admin@.(none)>
2012-11-08 21:39:31 +0100

    init

---
+++ src/Main.hx
@@ -0,0 +1,21 @@
+package ;
+
+import flash.Lib;
+
+/**
+ * ...
+ * @author de
+ */
+
+//todo http://philippseifried.com/blog/2011/07/24/flash-effects-creating-metaball-style-effects-in-as3/
+//todo http://philippseifried.com/blog/2011/07/30/real-time-bloom-effects-in-as3/
+
+class Main 
+{
+	
+	static function main() 
+	{
+		
+	}
+	
+}
